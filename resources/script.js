0f4c33a3b2b7b7b28e0b1842c7aec286496f2408
Andrea <andreaxis.95@gmail.com>
2023-07-01 16:59:51 +0200

    work in progress

--- resources/script.js
+++ resources/script.js
@@ -1,11 +1,34 @@
-const container = document.querySelector('.container')
-for (let index = 0; index <  16; index++) {
+const body = document.querySelector('body')
+let container = document.querySelector('.container')
+
+function animateGrid () {
+    const squares = document.querySelectorAll('.gridElement')
+squares.forEach(div => {
+    div.addEventListener('mousedown', e => {
+    e.target.classList.add('backgroundColor')
+})
+div.addEventListener('mouseout', e => {
+    e.target.addEventListener('transitionend', e => e.target.classList.remove('backgroundColor'))
+})})
+}
+
+
+function createGrid (x) {
+    container.remove()
+    container = document.createElement('div')
+    container.classList.add('container')
+    body.appendChild(container)
+for (let index = 0; index <  x; index++) {
     let row = document.createElement('div')
     row.classList.add('row')
     container.appendChild(row)
-    for (let index = 0; index < 16; index++) {
+    for (let index = 0; index < x; index++) {
         let gridElement = document.createElement('div')
         gridElement.classList.add('gridElement')
         row.appendChild(gridElement) 
     }
 }
+animateGrid()
+}
+
+createGrid(24)
