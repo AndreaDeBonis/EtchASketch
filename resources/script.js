1cf7048e91b492cafd9711c0e5322e8fcd8f3e23
Andrea <andreaxis.95@gmail.com>
2023-07-07 22:34:33 +0200

    feat: added color picker and rainbow mode

--- resources/script.js
+++ resources/script.js
@@ -1,30 +1,72 @@
 const body = document.querySelector('body')
+
+let currentGrid = 24
 let container = document.querySelector('.container')
 let color = 'black'
-let mouseDown = 0;
+let mouseDown = false;
+let rainbowMode = false;
+
+const gridSizeValue = document.getElementById('gridSizeValue')
+const gridSizeSlider = document.getElementById('gridSizeSlider')
+const colorPicker = document.getElementById('colorPicker')
+
 document.body.onmousedown = function() { 
-  mouseDown = 1;
+  mouseDown = true;
 }
 document.body.onmouseup = function() {
-  mouseDown = 0;
+  mouseDown = false;
 }
+
+gridSizeSlider.onmousemove = (e) => updateGridSizeValue(e.target.value)
+gridSizeSlider.onchange = (e) => createGrid(e.target.value)
+colorPicker.oninput = (e) => changeColor(e.target.value)
 
 function changeColor(newColor) {
-    color = newColor
+    if(newColor == 'rainbow') {
+        rainbowMode = true
+    }
+    else {
+        rainbowMode = false 
+        color = newColor
+    }
+   
 }
 
-function animateGrid () {
+function updateGridSizeValue(size) { 
+    gridSizeValue.innerHTML= `${size} x ${size}`
+}
+/* function animateGrid () {
     const squares = document.querySelectorAll('.gridElement')
 squares.forEach(div => {
     div.addEventListener('mouseover', e => {
-    if(mouseDown==1) {
+    if(mouseDown) {
     e.target.style.backgroundColor = color}
 })
+div.addEventListener('mousedown', e => {
+    e.target.style.backgroundColor = color
 })
-}
+})
+} */
 
+function activateGrid() {
+    const table = document.querySelector('.container')
+    table.addEventListener('mouseover', e => {
+        if(mouseDown) {
+            if(rainbowMode) {
+                color = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`
+            }
+        e.target.style.backgroundColor = color}
+    })
+    table.addEventListener('mousedown', e => {
+        if(rainbowMode) {
+            color = `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`
+        }
+        e.target.style.backgroundColor = color
+})}
+ 
 
-function createGrid (x) {
+function createGrid (x = currentGrid) {
+    currentGrid = x
     container.remove()
     container = document.createElement('div')
     container.classList.add('container')
@@ -37,9 +79,8 @@ for (let index = 0; index <  x; index++) {
         let gridElement = document.createElement('div')
         gridElement.classList.add('gridElement')
         row.appendChild(gridElement) 
-    }
-}
-animateGrid()
+    }}
+    activateGrid()
 }
 
-createGrid(24)
+createGrid(currentGrid)
